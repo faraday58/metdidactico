3c689b031a56d6e8f60b519331779e44d2567c99
faraday58 <jarodriguezvera@gmail.com>
2020-03-04 14:23:25 -0600

    Fragments, Listener Login y Registro

---
+++ app/src/main/java/com/example/metdidactico/Fragments/CarritoFragment.java
@@ -0,0 +1,22 @@
+package com.example.metdidactico.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.metdidactico.R;
+
+public class BuscarFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+         View v= inflater.inflate(R.layout.fragment_buscar,container,false);
+        return v ;
+    }
+}
